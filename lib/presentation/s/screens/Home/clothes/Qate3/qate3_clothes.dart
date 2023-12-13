ecca73577f49644316adfc981f1a61abd399312c
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-12-14 00:15:33 +0200

    Adding more products and category

--- lib/presentation/s/screens/Home/clothes/Qate3/qate3_clothes.dart
+++ lib/presentation/s/screens/Home/clothes/Qate3/qate3_clothes.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/cupertino.dart';
+import 'package:qate3_app/constants/qate3_model.dart';
 
 class Qate3Clothes extends StatelessWidget {
   const Qate3Clothes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Qate3Model(
+      itemCountCallback: (index) {
+        return 23;
+      },
+      barTitle: ' منتجات الملابس المقاطعة',
+      imagePath1: 'assets/clothes/Qate3/1.jpg',
+      imagePath2: 'assets/clothes/Qate3/2.jpg',
+      imagePath3: 'assets/clothes/Qate3/3.jpg',
+      imagePath4: 'assets/clothes/Qate3/4.jpg',
+      imagePath5: 'assets/clothes/Qate3/5.jpg',
+      imagePath6: 'assets/clothes/Qate3/6.jpg',
+      imagePath7: 'assets/clothes/Qate3/7.jpg',
+      imagePath8: 'assets/clothes/Qate3/8.jpg',
+      imagePath9: 'assets/clothes/Qate3/9.jpg',
+      imagePath10: 'assets/clothes/Qate3/10.jpg',
+      imagePath11: 'assets/clothes/Qate3/11.jpg',
+      imagePath12: 'assets/clothes/Qate3/12.jpg',
+      imagePath13: 'assets/clothes/Qate3/13.jpg',
+      imagePath14: 'assets/clothes/Qate3/14.jpg',
+      imagePath15: 'assets/clothes/Qate3/15.jpg',
+      imagePath16: 'assets/clothes/Qate3/16.jpg',
+      imagePath17: 'assets/clothes/Qate3/17.jpg',
+      imagePath18: 'assets/clothes/Qate3/18.jpg',
+      imagePath19: 'assets/clothes/Qate3/19.jpg',
+      imagePath20: 'assets/clothes/Qate3/20.jpg',
+      imagePath21: 'assets/clothes/Qate3/21.jpg',
+      imagePath22: 'assets/clothes/Qate3/22.jpg',
+      imagePath23: 'assets/clothes/Qate3/23.jpg',
+      title1: '',
+      title2: ' ',
+      title3: ' ',
+      title4: '',
+      title5: ' ',
+      title6: ' ',
+      title7: ' ',
+      title8: ' ',
+      title9: '',
+      title10: '',
+      title11: '',
+      title12: '',
+      title13: '',
+      title14: '',
+      title15: '',
+      title16: '',
+      title17: '',
+      title18: '',
+      title19: '',
+      title20: '',
+      title21: '',
+      title22: '',
+      title23: '',
+    );
   }
 }
