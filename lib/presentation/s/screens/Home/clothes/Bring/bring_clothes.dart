dd4098b41e395fb60b3c101f67dc675985f24edf
hamdyhaggag <hamdyhaggag74@gmail.com>
2023-12-14 00:49:19 +0200

    Adding more products and category

--- lib/presentation/s/screens/Home/clothes/Bring/bring_clothes.dart
+++ lib/presentation/s/screens/Home/clothes/Bring/bring_clothes.dart
@@ -8,21 +8,39 @@ class BringClothes extends StatelessWidget {
   Widget build(BuildContext context) {
     return BringModel(
       itemCountCallback: (index) {
-        return 6;
+        return 15;
       },
-      barTitle: ' محطات البنزين والزيوت البديلة',
+      barTitle: ' منتجات الملابس البديلة',
       imagePath1: 'assets/clothes/Bring/1.jpg',
       imagePath2: 'assets/clothes/Bring/2.jpg',
       imagePath3: 'assets/clothes/Bring/3.jpg',
       imagePath4: 'assets/clothes/Bring/4.jpg',
       imagePath5: 'assets/clothes/Bring/5.jpg',
       imagePath6: 'assets/clothes/Bring/6.jpg',
-      title1: 'مصر للبترول',
-      title2: 'شيلد ',
-      title3: 'سي بي سي ',
-      title4: 'الوطنية',
-      title5: 'تشل أوت ',
-      title6: 'التعاون للبترول',
+      imagePath7: 'assets/clothes/Bring/7.jpg',
+      imagePath8: 'assets/clothes/Bring/8.jpg',
+      imagePath9: 'assets/clothes/Bring/9.jpg',
+      imagePath10: 'assets/clothes/Bring/10.jpg',
+      imagePath11: 'assets/clothes/Bring/11.jpg',
+      imagePath12: 'assets/clothes/Bring/12.jpg',
+      imagePath13: 'assets/clothes/Bring/13.jpg',
+      imagePath14: 'assets/clothes/Bring/14.jpg',
+      imagePath15: 'assets/clothes/Bring/15.jpg',
+      title1: ' ',
+      title2: ' ',
+      title3: '   ',
+      title4: '',
+      title5: '  ',
+      title6: ' ',
+      title7: ' ',
+      title8: ' ',
+      title9: ' ',
+      title10: ' ',
+      title11: ' ',
+      title12: ' ',
+      title13: ' ',
+      title14: ' ',
+      title15: ' ',
     );
   }
 }
