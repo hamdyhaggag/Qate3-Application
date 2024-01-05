db2238ab3ead3e2a379298133709349304c7f444
hamdyhaggag <hamdyhaggag74@gmail.com>
2024-01-05 23:56:17 +0200

    version 1.0.0+10

--- lib/presentation/s/screens/Home/kitchen/Bring/bring_gebna.dart
+++ lib/presentation/s/screens/Home/kitchen/Bring/bring_gebna.dart
@@ -9,7 +9,7 @@ class BringGebna extends StatelessWidget {
   Widget build(BuildContext context) {
     return BringModel(
       itemCountCallback: (index) {
-        return 11;
+        return 10;
       },
       barTitle: ' منتجات الألبان البديلة',
       imagePath1: 'assets/kitchen/Bring/g1.jpg',
@@ -18,22 +18,20 @@ class BringGebna extends StatelessWidget {
       imagePath4: 'assets/kitchen/Bring/g4.jpg',
       imagePath5: 'assets/kitchen/Bring/g5.jpg',
       imagePath6: 'assets/kitchen/Bring/g6.jpg',
-      imagePath7: 'assets/kitchen/Bring/g7.jpg',
-      imagePath8: 'assets/kitchen/Bring/g8.jpg',
-      imagePath9: 'assets/kitchen/Bring/g9.jpg',
-      imagePath10: 'assets/kitchen/Bring/g10.jpg',
-      imagePath11: 'assets/kitchen/Bring/g11.jpg',
+      imagePath7: 'assets/kitchen/Bring/g8.jpg',
+      imagePath8: 'assets/kitchen/Bring/g9.jpg',
+      imagePath9: 'assets/kitchen/Bring/g10.jpg',
+      imagePath10: 'assets/kitchen/Bring/g11.jpg',
       title1: ' لبنيتا',
       title2: ' عبور لاند ',
       title3: 'تشيزا',
       title4: ' كاتيلو',
       title5: ' مزارع دينا ',
       title6: 'خير المصريين ',
-      title7: ' طعمه',
-      title8: 'هيلثي',
-      title9: 'رودس ',
-      title10: 'دومتي',
-      title11: 'ريماس لاند',
+      title7: 'هيلثي',
+      title8: 'رودس ',
+      title9: 'دومتي',
+      title10: 'ريماس لاند',
     );
   }
 }
