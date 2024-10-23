0ca3f6a761ac1c48064aab50eb3b71b5713aee7d
hamdy haggag <hamdyhaggag74@gmail.com>
2024-10-23 12:02:01 +0300

    Add Kafeta products

--- lib/presentation/screens/Home/Drinks/Bring/bring_coffee.dart
+++ lib/presentation/screens/Home/Drinks/Bring/bring_coffee.dart
@@ -8,15 +8,19 @@ class BringCoffee extends StatelessWidget {
   Widget build(BuildContext context) {
     return BringModel(
       itemCountCallback: (index) {
-        return 3;
+        return 5;
       },
       barTitle: ' منتجات القهوة البديلة',
-      imagePath1: 'assets/Drinks/Bring/c1.png',
+      imagePath1: 'assets/Drinks/Bring/c6.png',
       imagePath2: 'assets/Drinks/Bring/c2.png',
       imagePath3: 'assets/Drinks/Bring/c4.png',
-      title1: 'بن العروبة',
+      imagePath4: 'assets/Drinks/Bring/c1.png',
+      imagePath5: 'assets/Drinks/Bring/c5.png',
+      title1: 'كافيتا',
       title2: '  حاج عرفة',
       title3: ' عماد أفندي',
+      title4: ' بن العروبة',
+      title5: ' روز جاردن',
     );
   }
 }
--- lib/presentation/screens/Home/Drinks/Bring/bring_nescafe.dart
+++ lib/presentation/screens/Home/Drinks/Bring/bring_nescafe.dart
@@ -8,23 +8,25 @@ class BringNescafe extends StatelessWidget {
   Widget build(BuildContext context) {
     return BringModel(
       itemCountCallback: (index) {
-        return 7;
+        return 8;
       },
       barTitle: ' منتجات النسكافية البديلة',
       imagePath1: 'assets/Drinks/Bring/n1.png',
-      imagePath2: 'assets/Drinks/Bring/n2.png',
+      imagePath2: 'assets/Drinks/Bring/c6.png',
       imagePath3: 'assets/Drinks/Bring/n3.png',
       imagePath4: 'assets/Drinks/Bring/n4.png',
       imagePath5: 'assets/Drinks/Bring/n5.png',
       imagePath6: 'assets/Drinks/Bring/n6.png',
       imagePath7: 'assets/Drinks/Bring/n7.png',
+      imagePath8: 'assets/Drinks/Bring/n2.png',
       title1: 'علي كافية',
-      title2: ' رووتس ',
+      title2: ' كافيتا ',
       title3: 'ناتشي',
       title4: ' كوفي بريك',
       title5: ' بارلي ',
       title6: 'مصر كافية ',
       title7: '  ميجا كافية',
+      title8: '  رووتس',
     );
   }
 }
